98b4a7f5fc477af9660b45b61c2e546274ba74cf
WenjunWang-SCUT <body_and_mind@sohu.com>
2025-10-15 01:24:28 +0800

    Update pretrain_dap.py

--- main/pretrain_dap.py
+++ main/pretrain_dap.py
@@ -11,11 +11,12 @@ from configuration import config_init
 from frame import Learner
 
 
-def SL_train(config):
+def SL_pretrain(config):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(config.device)
 
     if config.model == 'FusionDNAbert':
-        config.kmers = [3, 6]
+        config.kmers = [3, 6] # dual-scale
+        # config.kmers = [3, 4, 5, 6] # four-scale
 
     learner = Learner.Learner(config)
     learner.setIO()
@@ -28,40 +29,6 @@ def SL_train(config):
 
 
 def select_dataset():
-    # path_train_data = '../data/DNA_MS/tsv/5hmC/5hmC_H.sapiens/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/5hmC/5hmC_H.sapiens/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/5hmC/5hmC_M.musculus/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/5hmC/5hmC_M.musculus/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/4mC/4mC_C.equisetifolia/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/4mC/4mC_C.equisetifolia/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/4mC/4mC_F.vesca/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/4mC/4mC_F.vesca/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/4mC/4mC_S.cerevisiae/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/4mC/4mC_S.cerevisiae/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/4mC/4mC_Tolypocladium/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/4mC/4mC_Tolypocladium/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_A.thaliana/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_A.thaliana/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_C.elegans/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_C.elegans/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_C.equisetifolia/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_C.equisetifolia/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_D.melanogaster/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_D.melanogaster/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_F.vesca/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_F.vesca/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_H.sapiens/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_H.sapiens/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_R.chinensis/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_R.chinensis/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_S.cerevisiae/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_S.cerevisiae/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_T.thermophile/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_T.thermophile/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_Tolypocladium/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_Tolypocladium/test.tsv'
-    # path_train_data = '../data/DNA_MS/tsv/6mA/6mA_Xoc BLS256/train.tsv'
-    # path_test_data = '../data/DNA_MS/tsv/6mA/6mA_Xoc BLS256/test.tsv'
     train_dict = {
         "4mCF": '../data/DNA_MS/tsv/4mC/4mC_F.vesca/train.tsv',
         "4mCS": '../data/DNA_MS/tsv/4mC/4mC_S.cerevisiae/train.tsv',
@@ -91,7 +58,7 @@ if __name__ == '__main__':
     # start_time = time.time()
     config = config_init.get_config()
     config.path_train_data = select_dataset()
-    SL_train(config)
+    SL_pretrain(config)
     # end_time = time.time()
     # total_time = end_time - start_time
     # print(f"Total pretraining time: {total_time/3600:.2f} h")
