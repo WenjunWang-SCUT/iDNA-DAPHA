d13d34caed6be40fe2f0b3597c66daa23322cdb1
WenjunWang-SCUT <body_and_mind@sohu.com>
2025-10-14 11:44:11 +0800

    Update FusionDNAbert.py

--- model/FusionDNAbert.py
+++ model/FusionDNAbert.py
@@ -19,29 +19,6 @@ class FusionBERT(nn.Module):
         self.config.kmer = self.config.kmers[1]
         self.berttwo = DNAbert.BERT(self.config)
         
-        # self.transformer = TransformerF.Transformer(
-        #         src_vocab_size=64,
-        #         src_pad_idx=0,
-        #         max_length=41
-        #         )#0.1
-        # with open("../model/config.json", encoding='utf-8', errors='ignore') as json_data:
-        #     config = json.load(json_data, strict=False)
-        # att_DNA_args_3mer = {'n_nts': config['MODEL']['embedding_dim'], 'n_bins': 39,
-        #                      'bin_rnn_size': config['MODEL']['hidden_dim'],
-        #                      'num_layers': config['MODEL']['hidden_layers'],
-        #                      'dropout': 0.1, 'bidirectional': True}
-        # self.att_DNA_3mer = Lstm_atta_models.att_DNA(att_DNA_args_3mer, config['MODEL']['output_dim'])
-        #
-        # att_DNA_args_6mer = {'n_nts': config['MODEL']['embedding_dim'], 'n_bins': 36,
-        #                 'bin_rnn_size': config['MODEL']['hidden_dim'], 'num_layers': config['MODEL']['hidden_layers'],
-        #                 'dropout': 0.1, 'bidirectional': True}
-        # self.att_DNA_6mer = Lstm_atta_models.att_DNA(att_DNA_args_6mer, config['MODEL']['output_dim'])
-
-        # self.lstm = nn.LSTM(input_size = 768, hidden_size = 256, num_layers = 2,
-        #                     batch_first=True, bidirectional=True)
-        # self.attention = nn.MultiheadAttention(embed_dim = 256*2, num_heads = 8)
-        # self.kalman_filter = KalmanFilter(n_dim_obs=512, n_dim_state=512)
-
         self.classification = nn.Sequential(
             nn.Linear(768 * 2, 20),
             nn.Dropout(0.5),
